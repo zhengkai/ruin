38dc914e5449cbc497e345f8215e14cdc3e83ee5
Zheng Kai <zhengkai@gmail.com>
2025-12-04 18:28:08 +0800

    build win

--- src/sys/ver.hpp
+++ src/sys/ver.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include "../config.hpp"
 #include "box2d/base.h"
 #include "build.h"
 #include <SDL3/SDL.h>
@@ -10,28 +11,37 @@
 #include <cstring>
 #include <spdlog/spdlog.h>
 
-static void doShowVer() {
-	spdlog::info("SDL       = {}.{}.{}",
+inline static void doShowVer() {
+	spdlog::info("SDL        = {}.{}.{}",
 		SDL_MAJOR_VERSION,
 		SDL_MINOR_VERSION,
 		SDL_MICRO_VERSION);
 #ifndef __EMSCRIPTEN__
-	spdlog::info("SDL ttf   = {}.{}.{}",
+	spdlog::info("SDL ttf    = {}.{}.{}",
 		SDL_TTF_MAJOR_VERSION,
 		SDL_TTF_MINOR_VERSION,
 		SDL_TTF_MICRO_VERSION);
 #endif
-	spdlog::info("SDL image = {}.{}.{}",
+	spdlog::info("SDL image  = {}.{}.{}",
 		SDL_IMAGE_MAJOR_VERSION,
 		SDL_IMAGE_MINOR_VERSION,
 		SDL_IMAGE_MICRO_VERSION);
 
 	b2Version v = b2GetVersion();
-	spdlog::info("Box2D     = {}.{}.{}", v.major, v.minor, v.revision);
+	spdlog::info("Box2D      = {}.{}.{}", v.major, v.minor, v.revision);
 
-	spdlog::info("git = {}({}{})",
+	spdlog::info("git        = {}({}{})",
 		GIT_BRANCH,
 		GIT_HASH,
 		(strcmp(GIT_DIRTY, "yes") == 0) ? "-dirty" : "");
 	spdlog::info("build time = {}", BUILD_TIMESTAMP);
+
+	std::string s;
+	for (const auto &p : config::dirList) {
+		if (s != "") {
+			s += ", ";
+		}
+		s += p.string();
+	}
+	spdlog::info("dir        = {}", s);
 }
