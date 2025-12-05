e6abd909897b6d8dae30a8361c02fcb954bf680a
Zheng Kai <zhengkai@gmail.com>
2025-12-05 14:19:53 +0800

    win release asset

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-reset
+ruin.one 废墟一号
+======
+
+Windows 版 每日构建：  
+<https://github.com/zhengkai/ruin/releases/download/daily/ruin-win.zip>
---
+++ misc/copy-asset.mjs
@@ -0,0 +1,71 @@
+import fs from "fs";
+import path from "path";
+import { dirname } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const rootDir = dirname(dirname(fileURLToPath(import.meta.url)));
+
+const SOURCE = "asset";
+const MANIFEST = "manifest.json";
+
+const dstDir = path.join(rootDir, "build", "Release");
+
+function walk(obj, cb) {
+	if (Array.isArray(obj)) {
+		return obj.forEach(i => walk(i, cb));
+	}
+	if (obj && typeof obj === "object") {
+		if (obj.path) {
+			cb(obj.path);
+		}
+		for (const k in obj) {
+			walk(obj[k], cb);
+		}
+	}
+}
+
+(() => {
+
+	const srcDir = path.join(rootDir, SOURCE);
+
+	if (!fs.existsSync(srcDir)) {
+		console.error("asset dir not found:", srcDir);
+		return;
+	}
+
+
+	if (!fs.existsSync(dstDir)) {
+		console.error("target dir not found:", dstDir);
+		return;
+	}
+
+	const manifestFile = path.join(srcDir, MANIFEST);
+	if (!fs.existsSync(manifestFile)) {
+		console.error("Manifest file not found:", manifestFile);
+		return;
+	}
+
+	let manifest;
+	try {
+		manifest = JSON.parse(fs.readFileSync(manifestFile, "utf8"));
+	} catch (e) {
+		console.error(`Manifest file ${manifestFile} read fail: ${e.message}`);
+	}
+
+	const copy = (p) => {
+		const src = path.join(srcDir, p);
+		const dst = path.join(dstDir, SOURCE, p);
+
+		fs.mkdirSync(path.dirname(dst), { recursive: true });
+		fs.copyFileSync(src, dst);
+
+		console.log("copied", src, "=>", dst);
+	}
+
+	walk(manifest, copy);
+	copy(MANIFEST);
+	copy("map.json")
+	copy("pong/circle.webp");
+	copy("font/JetBrainsMono-Regular.otf");
+
+})();
