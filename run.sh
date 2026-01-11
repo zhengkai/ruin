eeca928eda14158ff408a86638bbbe74d58fe3a6
Zheng Kai <zhengkai@gmail.com>
2026-01-11 12:37:27 +0800

    chore: ccache

--- run.sh
+++ run.sh
@@ -13,15 +13,25 @@ fi
 export ASAN_OPTIONS=detect_leaks=0
 # export LSAN_OPTIONS="fast_unwind_on_malloc=0:verbosity=1:ignorelist=${PWD}/misc/lsan-ignore.txt"
 
-export CC=clang
-export CXX=clang++
+export CC="clang"
+export CXX="clang++"
+export CCACHE_NOCOMPRESS=true
+export CCACHE_DIRECT=1
+export CCACHE_MAXSIZE=50G
+export CMAKE_GENERATOR=Ninja
 
 cmake \
+	-S . \
 	-B build \
+	-G Ninja \
+	-DCMAKE_C_COMPILER=clang \
+	-DCMAKE_CXX_COMPILER=clang++ \
+	-DCMAKE_C_COMPILER_LAUNCHER=ccache \
+	-DCMAKE_CXX_COMPILER_LAUNCHER=ccache \
 	-DCMAKE_TOOLCHAIN_FILE="${PWD}/tool/vcpkg/scripts/buildsystems/vcpkg.cmake" \
 	-DCMAKE_BUILD_TYPE="$BUILD_TYPE"
 
-cmake --build build --config "$BUILD_TYPE" -j"$(nproc)"
+cmake --build build -j"$(nproc)"
 
 ./ldd/run.sh
 
