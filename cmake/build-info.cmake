cacb3d852a3c0a396ee3cc7cb6615ee2a4b0bf35
Zheng Kai <zhengkai@gmail.com>
2026-04-26 14:30:18 +0800

    build: fix cmake message

--- cmake/build-info.cmake
+++ cmake/build-info.cmake
@@ -1,34 +1,39 @@
 find_package(Git QUIET)
 
-set(GIT_BRANCH "unknown")
-set(GIT_HASH "unknown")
+
+set(GIT_BRANCH "")
+set(GIT_HASH   "git-not-found")
+set(GIT_DIRTY  "")
 if(GIT_FOUND)
-	execute_process(
-		COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
-		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-		OUTPUT_VARIABLE GIT_BRANCH
-		OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-	execute_process(
-		COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
-		WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-		OUTPUT_VARIABLE GIT_HASH
-		OUTPUT_STRIP_TRAILING_WHITESPACE
-	)
-endif()
-
-
-set(GIT_DIRTY "unknown")
-execute_process(
-	COMMAND ${GIT_EXECUTABLE} status --porcelain
-	WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
-	OUTPUT_VARIABLE GIT_STATUS_OUTPUT
-	OUTPUT_STRIP_TRAILING_WHITESPACE
-)
-if("${GIT_STATUS_OUTPUT}" STREQUAL "")
-	set(GIT_DIRTY "no")
-else()
-	set(GIT_DIRTY "yes")
+	if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+		set(GIT_BRANCH "unknown")
+		set(GIT_HASH "unknown")
+		execute_process(
+			COMMAND ${GIT_EXECUTABLE} rev-parse --abbrev-ref HEAD
+			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+			OUTPUT_VARIABLE GIT_BRANCH
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		execute_process(
+			COMMAND ${GIT_EXECUTABLE} log -1 --format=%h
+			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+			OUTPUT_VARIABLE GIT_HASH
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		execute_process(
+			COMMAND ${GIT_EXECUTABLE} status --porcelain
+			WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+			OUTPUT_VARIABLE GIT_STATUS_OUTPUT
+			OUTPUT_STRIP_TRAILING_WHITESPACE
+		)
+		if("${GIT_STATUS_OUTPUT}" STREQUAL "")
+			set(GIT_DIRTY "no")
+		else()
+			set(GIT_DIRTY "yes")
+		endif()
+	else()
+		set(GIT_HASH   "not-a-git-repository")
+	endif()
 endif()
 
 unset(ENV{SOURCE_DATE_EPOCH})
